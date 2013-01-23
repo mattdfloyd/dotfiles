497488b7194d1484925af007a4b0eedf610d8e45
Matt Floyd <mattdfloyd@gmail.com>
2013-01-23 01:19:04 -0500

    use DOTFILES instead of ZSH

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export PATH=":/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/share/npm/bin:$HOME/.rvm/bin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH=":/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/usr/local/share/npm/bin:$HOME/.rvm/bin:$HOME/.sfs:$DOTFILES/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
